bb0887b3fab0880da94620c042161618d334f28c
Deepak Sinha <sinhadeepak1115study@gmail.com>
2021-12-04 15:22:36 +0530

    final modification full functional.

--- main.py
+++ main.py
@@ -1,26 +1,30 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField ,SubmitField
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
+from flask_bootstrap import Bootstrap
+
 
 class LoginForm(FlaskForm):
     email = StringField(label='Email', validators=[DataRequired(), Email()])
     password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8)])
     submit = SubmitField(label="Log In")
-    
+
+
 app = Flask(__name__)
-app.secret_key = "deepak"
+app.secret_key = "any-string-you-want-just-keep-it-secret"
+Bootstrap(app)
 
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
     login_form = LoginForm()
     if login_form.validate_on_submit():
-        print(f"{login_form.email.data}\n{login_form.password.data}")
         if login_form.email.data == "sinhadeepak1115study@gmail.com" and login_form.password.data == "123456789":
             return render_template("success.html")
         else:
